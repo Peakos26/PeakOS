6f837289a0da8c5a455eff8fc22fe28fd17e6164
Helton Sales <heltonsales@icloud.com>
2026-06-07 10:37:00 -0300

    Deploy v1.0.9: Sprint 8 - PWA e Integrações

---
+++ dist/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'peakos-v1.0.8'
+const urlsToCache = [
+  '/PeakOS/',
+  '/PeakOS/index.html',
+  '/PeakOS/manifest.json',
+  '/PeakOS/assets/index-14de5d4f.css',
+  '/PeakOS/assets/index-0b5bd240.js'
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response
+        }
+        return fetch(event.request)
+      })
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+})
